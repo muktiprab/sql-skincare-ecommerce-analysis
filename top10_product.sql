9a51b04b18e3d6d63f1f4cce633794df71bd1749
Muktiprab007 <mukti.prabowo56@gmail.com>
2026-06-10 17:08:03 +0700

    Top 10 Fixed

--- top10_product.sql
+++ top10_product.sql
@@ -3,13 +3,14 @@ SELECT TOP 10
 	p.product_id,
 	p.product_name,
 	p.category,
+	oi.unit_price,
 	SUM(oi.quantity) AS total_qty_sold,
-	SUM(oi.item_total) AS total_revenue,
-	AVG(oi.unit_price) AS avg_unit_price
+	SUM(oi.item_total) AS total_revenue
 FROM order_items oi
 JOIN products p ON oi.product_id = p.product_id
 GROUP BY
 	p.product_id,
 	p.product_name,
-	p.category
+	p.category,
+	unit_price
 ORDER BY total_qty_sold DESC;
