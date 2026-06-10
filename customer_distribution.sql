f34c109611544fdf5022ded58ade854050c439c3
Muktiprab007 <mukti.prabowo56@gmail.com>
2026-06-10 17:47:09 +0700

    Add Customer Distrbution Fixed

--- customer_distribution.sql
+++ customer_distribution.sql
@@ -12,9 +12,23 @@ FROM customers
 GROUP BY age_group
 ORDER BY total_customers DESC;
 
--- Customers Distribution by Area
+-- Customers Distribution by State
 SELECT state,
 	COUNT(customer_id) AS total_customers
 FROM customers
 GROUP BY state
+ORDER BY total_customers DESC;
+
+-- Customers Distribution by City 
+SELECT city,
+	COUNT(customer_id) AS total_customers
+FROM customers
+GROUP BY city
+ORDER BY total_customers DESC;
+
+-- Customers Distribution by Acquisition Channel
+SELECT acquisition_channel,
+	COUNT(customer_id) AS total_customers
+FROM customers
+GROUP BY acquisition_channel
 ORDER BY total_customers DESC;
